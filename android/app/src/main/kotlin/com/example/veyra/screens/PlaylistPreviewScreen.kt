04378b5fef550b32320efca4d2936ee9891cf711
Hugo Heilmann <147172763+HugoHeilmann@users.noreply.github.com>
2026-05-08 12:49:27 +0200

    fix: musicrow inside a playlist restrain queue to playlist songs

--- android/app/src/main/kotlin/com/example/veyra/screens/PlaylistPreviewScreen.kt
+++ android/app/src/main/kotlin/com/example/veyra/screens/PlaylistPreviewScreen.kt
@@ -180,7 +180,7 @@ fun PlaylistPreviewScreen(
                                 .padding(vertical = 8.dp, horizontal = 16.dp),
                             onClick = {
                                 MusicHolder.isShuffled = true
-                                MusicHolder.setCurrentMusic(context, music, "Playlist: $playlistName", allSongs)
+                                MusicHolder.setCurrentMusic(context, music, "Playlist: $playlistName", playlistSongs)
                                 navController.navigate("player")
                             },
                             onEditClick = { _ ->
